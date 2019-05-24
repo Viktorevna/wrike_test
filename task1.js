da7b7e70f2f969120f133cf44d4b78c8642a266a
Anastasia <nast.cali.2013@yandex.ru>
2019-05-24 22:16:17 +0300

    add task1

---
+++ task1.js
@@ -0,0 +1,32 @@
+function sortWithSavePositions(array) {
+  let newArray = array.map((item, i, arr) => {
+    return [i, arr[i]]
+  });
+  newArray.sort((a,b) => {
+    if (a[1] > b[1])
+      return 1;
+    if (a[1] < b[1])
+      return -1;
+    return 0;
+  });
+  return newArray;
+}
+
+
+function binarySearch(array, element){
+  let newArray = sortWithSavePositions(array);
+  let newElement = null;
+  let leftBorder = 0;
+  let rightBorder = newArray.length - 1;
+  while (leftBorder < rightBorder) {
+    let middle = Math.floor(leftBorder + ((rightBorder - leftBorder) / 2));
+    newElement = newArray[middle][1];
+    if (newElement < element)
+      leftBorder = middle + 1;
+    else if (newElement > element)
+      rightBorder = middle - 1;
+    else
+      return newArray[middle][0]
+  }
+  return -1;
+}
