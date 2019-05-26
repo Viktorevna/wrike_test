9737e1837ff4c1ba471f6eddf5282be3da4ce171
Anastasia <nast.cali.2013@yandex.ru>
2019-05-26 14:32:36 +0300

    fixed bugs & add comments

--- task2/task2.js
+++ task2/task2.js
@@ -5,73 +5,15 @@ const $result = document.querySelector(".result");
 let userSign = "x";
 let computerSign = "o";
 let winningCombonations = ['012','345','678','036','147','258','048','246'];
-let breakFlag = false;
-let winFlag = false;
+let breakFlag = false; //флаг нахождения оптимального хода компьютера
+let winFlag = false; //флаг наличия победной компбинации
 
 let whoGoesFirst = Math.round(Math.random());
-let firstBlock = Math.round((Math.random() * 8));
+let firstBlock = Math.round((Math.random() * 8)); //опредление первого хода
 if (whoGoesFirst === 1) {
   userSign = "o";
   computerSign = "x";
   $block[firstBlock].textContent = computerSign;
-};
-
-function defineComputerBlock() {
-  for (let i = 0; i < winningCombonations.length; i++) {
-    let first = winningCombonations[i].slice(0, 1);
-    let second = winningCombonations[i].slice(1, 2);
-    let third = winningCombonations[i].slice(2, 3);
-    if ($block[first].textContent === computerSign && $block[second].textContent === computerSign && $block[third].textContent === "") {
-      $block[third].textContent = computerSign;
-      breakFlag = true;
-      break;
-    };
-    if ($block[first].textContent === computerSign && $block[third].textContent === computerSign && $block[second].textContent === "") {
-      $block[second].textContent = computerSign;
-      breakFlag = true;
-      break;
-    };
-    if ($block[second].textContent === computerSign && $block[third].textContent === computerSign && $block[first].textContent === "") {
-      $block[first].textContent = computerSign;
-      breakFlag = true;
-      break;
-    };
-    if ($block[first].textContent === userSign && $block[second].textContent === userSign && $block[third].textContent === "") {
-      $block[third].textContent = computerSign;
-      breakFlag = true;
-      break;
-    };
-    if ($block[first].textContent === userSign && $block[third].textContent === userSign && $block[second].textContent === "") {
-      $block[second].textContent = computerSign;
-      breakFlag = true;
-      break;
-    };
-    if ($block[second].textContent === userSign && $block[third].textContent === userSign && $block[first].textContent === "") {
-      $block[first].textContent = computerSign;
-      breakFlag = true;
-      break;
-    };
-  };
-  if (breakFlag === false)
-    for (let i = 0; i < $block.length; i++)
-      if ($block[i].textContent === "") {
-        $block[i].textContent = computerSign;
-        breakFlag = true;
-        break;
-      };
-};
-
-function isComputerAWinner() {
-  for (let i = 0; i < winningCombonations.length; i++) {
-    let first = winningCombonations[i].slice(0, 1);
-    let second = winningCombonations[i].slice(1, 2);
-    let third = winningCombonations[i].slice(2, 3);
-    if ($block[first].textContent === computerSign && $block[second].textContent === computerSign && $block[third].textContent === computerSign) {
-      $result.textContent = "Computer won";
-      winFlag = true;
-      break;
-    };
-  };
 };
 
 function isUserAWinner() {
@@ -87,17 +29,101 @@ function isUserAWinner() {
   };
 };
 
-for (let i = 0; i < $block.length; i++) {
-  $block[i].addEventListener("click", function(){
-  breakFlag = false;
-    if ($block[i].textContent === "" && winFlag === false) {
-      $block[i].textContent = userSign;
-      defineComputerBlock();
-      isUserAWinner();
-      isComputerAWinner();
+function isComputerCombinationWinning() {
+  if (winFlag === false)
+    for (let i = 0; i < winningCombonations.length && breakFlag === false; i++) {
+      let first = winningCombonations[i].slice(0, 1);
+      let second = winningCombonations[i].slice(1, 2);
+      let third = winningCombonations[i].slice(2, 3);
+      if ($block[first].textContent === computerSign && $block[second].textContent === computerSign && $block[third].textContent === "") {
+        $block[third].textContent = computerSign;
+        breakFlag = true;
+      }
+      else if ($block[first].textContent === computerSign && $block[third].textContent === computerSign && $block[second].textContent === "") {
+        $block[second].textContent = computerSign;
+        breakFlag = true;
+      }
+      else if ($block[second].textContent === computerSign && $block[third].textContent === computerSign && $block[first].textContent === "") {
+        $block[first].textContent = computerSign;
+        breakFlag = true;
+      };
     };
-  });
+}
+
+function isUserCombinationWinning() {
+  if (winFlag === false)
+    for (let i = 0; i < winningCombonations.length && breakFlag === false; i++) {
+      let first = winningCombonations[i].slice(0, 1);
+      let second = winningCombonations[i].slice(1, 2);
+      let third = winningCombonations[i].slice(2, 3);
+      if ($block[first].textContent === userSign && $block[second].textContent === userSign && $block[third].textContent === "") {
+        $block[third].textContent = computerSign;
+        breakFlag = true;
+      }
+      else if ($block[first].textContent === userSign && $block[third].textContent === userSign && $block[second].textContent === "") {
+        $block[second].textContent = computerSign;
+        breakFlag = true;
+      }
+      else if ($block[second].textContent === userSign && $block[third].textContent === userSign && $block[first].textContent === "") {
+        $block[first].textContent = computerSign;
+        breakFlag = true;
+      };
+    };
+}
+
+function defineComputerBlock() {
+  if (breakFlag === false && winFlag === false)
+    for (let i = 0; i < $block.length; i++)
+      if ($block[i].textContent === "") {
+        $block[i].textContent = computerSign;
+        breakFlag = true;
+        break;
+      };
 };
+
+function isComputerAWinner() {
+  if (winFlag === false)
+    for (let i = 0; i < winningCombonations.length; i++) {
+      let first = winningCombonations[i].slice(0, 1);
+      let second = winningCombonations[i].slice(1, 2);
+      let third = winningCombonations[i].slice(2, 3);
+      if ($block[first].textContent === computerSign && $block[second].textContent === computerSign && $block[third].textContent === computerSign) {
+        $result.textContent = "Computer won";
+        winFlag = true;
+        break;
+      };
+  };
+};
+
+// for (let i = 0; i < $block.length; i++) {
+//   $block[i].addEventListener("click", function(){
+//     breakFlag = false;
+//     console.log("click");
+//     if ($block[i].textContent === "" && winFlag === false) {
+//       $block[i].textContent = userSign; //если выбранное пользователем место свободно, то на него ставится пользовательский знак
+//       isUserAWinner(); //если три пользовательских знака стоят в ряд, то пользователь победил
+//       isComputerCombinationWinning(); //если условие предыдущей функции не выполнено и если два знака компьютера стоят в ряд, а на месте третьего пусто, то на пустое место ставится знак компьютера
+//       isUserCombinationWinning(); //если условия предыдущих функций не выполнилось и если два пользовательских знака стоят в ряд, а на месте третьего пусто, то на пустое место ставится знак компьютера
+//       defineComputerBlock(); //если условия предыдущих функций не выполнилось, то знак компьютера ставится на первое свободное место
+//       isComputerAWinner(); //если три знака компьютера стоят в ряд, то компьютер победил
+//     };
+//   });
+// };
+
+$block.forEach(item => {
+  item.addEventListener("click", function(){
+    breakFlag = false;
+    console.log("clifvfvck");
+    if (item.textContent === "" && winFlag === false) {
+      item.textContent = userSign; //если выбранное пользователем место свободно, то на него ставится пользовательский знак
+      isUserAWinner(); //если три пользовательских знака стоят в ряд, то пользователь победил
+      isComputerCombinationWinning(); //если условие предыдущей функции не выполнено и если два знака компьютера стоят в ряд, а на месте третьего пусто, то на пустое место ставится знак компьютера
+      isUserCombinationWinning(); //если условия предыдущих функций не выполнилось и если два пользовательских знака стоят в ряд, а на месте третьего пусто, то на пустое место ставится знак компьютера
+      defineComputerBlock(); //если условия предыдущих функций не выполнилось, то знак компьютера ставится на первое свободное место
+      isComputerAWinner(); //если три знака компьютера стоят в ряд, то компьютер победил
+    };
+  })
+})
 
 $button.addEventListener("click", function(){
   window.location.reload();
